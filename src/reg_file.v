124fe75154d7ddcdc059d4e9e45afc7802f37a0a
particlecluster <patiljeet7777@gmail.com>
2026-06-03 13:02:37 +0530

    Add register file module for read and write operations

---
+++ src/reg_file.v
@@ -0,0 +1,31 @@
+module reg_file(
+
+    input clk,
+
+    input [2:0] rs1,
+    input [2:0] rs2,
+
+    input [2:0] rd,
+    input [7:0] wd,
+
+    input reg_write, // write enable
+
+    output [7:0] rd1,
+    output [7:0] rd2
+
+);
+
+reg [7:0] regs [0:7];
+
+assign rd1 = (rs1 == 3'd0) ? 8'd0 : regs[rs1];
+assign rd2 = (rs2 == 3'd0) ? 8'd0 : regs[rs2];
+
+always @(posedge clk)
+begin
+
+    if(reg_write && rd != 3'd0)
+        regs[rd] <= wd;
+
+end
+
+endmodule
