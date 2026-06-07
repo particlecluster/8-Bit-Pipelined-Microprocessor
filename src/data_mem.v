0e118254868c3de1caeb7435a15ae71b09ce24b9
particlecluster <patiljeet7777@gmail.com>
2026-06-07 07:50:47 +0530

    Refactor data_mem module to include reset input

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,29 +1,27 @@
 module data_mem(
-
-    input clk,
-
-    input mem_read,
-    input mem_write,
-
-    input [7:0] addr,
-
-    input [7:0] write_data,
-
+    input        clk,
+    input        rst,
+    input        mem_read,
+    input        mem_write,
+    input  [7:0] addr,
+    input  [7:0] write_data,
     output [7:0] read_data
-
 );
 
 reg [7:0] mem [0:255];
 
-assign read_data =
-        (mem_read) ? mem[addr] : 8'b0;
+assign read_data = (mem_read) ? mem[addr] : 8'b0;
 
-always @(posedge clk)
-begin
+integer i;
 
-    if(mem_write)
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        for (i = 0; i < 256; i = i + 1)
+            mem[i] <= 8'b0;
+    end
+    else if (mem_write) begin
         mem[addr] <= write_data;
-
+    end
 end
 
 endmodule
