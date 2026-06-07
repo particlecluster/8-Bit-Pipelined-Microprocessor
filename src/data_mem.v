19c1b96c9133506a40c40703bb46ccf857771684
particlecluster <patiljeet7777@gmail.com>
2026-06-07 07:57:08 +0530

    Refactor data_mem module to remove reset logic
    Removed reset functionality and initialization block.

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,6 +1,5 @@
 module data_mem(
     input        clk,
-    input        rst,
     input        mem_read,
     input        mem_write,
     input  [7:0] addr,
@@ -9,17 +8,18 @@ module data_mem(
 );
 
 reg [7:0] mem [0:255];
+integer i;
+
+initial begin
+    for(i = 0; i < 256; i = i + 1)
+        mem[i] = 8'b0;
+end
 
 assign read_data = (mem_read) ? mem[addr] : 8'b0;
 
-integer i;
 
-always @(posedge clk or posedge rst) begin
-    if (rst) begin
-        for (i = 0; i < 256; i = i + 1)
-            mem[i] <= 8'b0;
-    end
-    else if (mem_write) begin
+always @(posedge clk) begin
+    if (mem_write) begin
         mem[addr] <= write_data;
     end
 end
