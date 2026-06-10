1c9e07fa398d815491909daef9356de698f4b6fe
particlecluster <patiljeet7777@gmail.com>
2026-06-10 23:51:58 +0530

    Enhance register file with additional inputs and outputs
    Added input for rs3 and output for rd3 to the register file.

--- src/reg_file.v
+++ src/reg_file.v
@@ -5,6 +5,7 @@ module reg_file(
 
     input [2:0] rs1,
     input [2:0] rs2,
+    input [2:0] rs3,
 
     input [2:0] rd,
     input [7:0] wd,
@@ -12,7 +13,8 @@ module reg_file(
     input reg_write, // write enable
 
     output [7:0] rd1,
-    output [7:0] rd2
+    output [7:0] rd2,
+    output [7:0] rd3
 
 );
 
@@ -20,6 +22,7 @@ reg [7:0] regs [0:7];
 
 assign rd1 = (rs1 == 3'd0) ? 8'd0 : regs[rs1];
 assign rd2 = (rs2 == 3'd0) ? 8'd0 : regs[rs2];
+assign rd3 = (rs3 == 3'd0) ? 8'd0 : regs[rs3];
 
 integer i;
 
