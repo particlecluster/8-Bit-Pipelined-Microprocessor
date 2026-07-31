8bf486ece42d037e62e44eb9637927c0e5a69a60
Jeet Patil <patiljeet7777@gmail.com>
2026-07-31 20:29:14 +0530

    Update instr_memory.v

--- src/instr_memory.v
+++ src/instr_memory.v
@@ -1,14 +1,10 @@
-`timescale 1ns / 1ps
-
-module InstructionMemory (
-    input  wire [7:0]  pc,
-    output wire [15:0] instr
+module InstructionMemory #(
+    parameter integer ADDR_W  = 8,
+    parameter integer INSTR_W = 16
+) (
+    input  wire [ADDR_W-1:0]  pc,
+    output wire [INSTR_W-1:0] instr
 );
-    reg [15:0] memory [0:255];
-
-    initial begin
-        $readmemh("program.hex", memory);
-    end
-
+    reg [INSTR_W-1:0] memory [0:(1<<ADDR_W)-1];
     assign instr = memory[pc];
 endmodule
