775b3163d0db7d17ac84f56b452f95914854d031
Jeet Patil <patiljeet7777@gmail.com>
2026-07-31 20:30:34 +0530

    Update extender.v

--- src/extender.v
+++ src/extender.v
@@ -1,10 +1,13 @@
 `timescape 1ns / 1ps
 
-module Extender (
-    input  wire [4:0] imm5,
-    output wire [7:0] imm5_sext,
-    output wire [7:0] imm5_zext
+module Extender #(
+    parameter integer DATA_W = 8,
+    parameter integer IMM5_W = 5
+) (
+    input  wire [IMM5_W-1:0] imm5,
+    output wire [DATA_W-1:0] imm5_sext,
+    output wire [DATA_W-1:0] imm5_zext
 );
-    assign imm5_sext = {{3{imm5[4]}}, imm5};
-    assign imm5_zext = {3'b000, imm5};
+    assign imm5_sext = {{(DATA_W-IMM5_W){imm5[IMM5_W-1]}}, imm5};
+    assign imm5_zext = {{(DATA_W-IMM5_W){1'b0}}, imm5};
 endmodule
